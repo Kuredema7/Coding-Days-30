37ad1d6a115e4c37047043e3687bc0962f356d28
moaliyou <moaliyou47@gmail.com>
2023-10-09 13:14:54 +0300

    Modified Typography to my own custom one

--- app/src/main/java/com/example/codingdays/ui/theme/Type.kt
+++ app/src/main/java/com/example/codingdays/ui/theme/Type.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.unit.sp
 import com.example.codingdays.R
 
@@ -14,13 +15,6 @@ val NotoSans = FontFamily(
 )
 
 val DailyGoalTypography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = NotoSans,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    ),
     displayLarge = TextStyle(
         fontFamily = NotoSans,
         fontWeight = FontWeight.Normal,
@@ -29,11 +23,32 @@ val DailyGoalTypography = Typography(
     displayMedium = TextStyle(
         fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
-        fontSize = 20.sp
+        fontSize = 24.sp
     ),
     displaySmall = TextStyle(
         fontFamily = NotoSans,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
-    )
+    ),
+    titleSmall = TextStyle(
+        fontFamily = NotoSans,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.1.sp,
+        fontWeight = FontWeight.Medium
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = NotoSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = NotoSans,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.25.sp,
+        lineBreak = LineBreak.Paragraph
+    ),
 )

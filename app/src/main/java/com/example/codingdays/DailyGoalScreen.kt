e3e97d5e6e1e4401ae539b189c544dc14fd9e78f
moaliyou <moaliyou47@gmail.com>
2023-10-10 09:50:03 +0300

    Added animation to the card to show daily goal description

--- app/src/main/java/com/example/codingdays/DailyGoalScreen.kt
+++ app/src/main/java/com/example/codingdays/DailyGoalScreen.kt
@@ -2,12 +2,14 @@ package com.example.codingdays
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,14 +18,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.CalendarToday
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -34,22 +40,46 @@ import com.example.codingdays.ui.theme.CodingDaysTheme
 import com.example.codingdays.ui.theme.DailyGaolShapes
 import com.example.codingdays.ui.theme.DailyGoalTypography
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DailyGoalCard(
-    dailyGoal: DailyGoal,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    dailyGoal: DailyGoal
 ) {
+    var showDayGaolDescription by remember {
+        mutableStateOf(false)
+    }
     Card(
         colors = CardDefaults.cardColors(MaterialTheme.colorScheme.surfaceVariant),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = dimensionResource(R.dimen.card_elevation_small)
+        ),
         modifier = Modifier
-            .padding(dimensionResource(R.dimen.padding_small))
+            .padding(
+                top = dimensionResource(R.dimen.padding_small),
+                bottom = dimensionResource(R.dimen.padding_small),
+                start = dimensionResource(R.dimen.padding_medium),
+                end = dimensionResource(R.dimen.padding_medium)
+
+            ),
+        onClick = {
+            showDayGaolDescription = !showDayGaolDescription
+        }
     ) {
         DailyGoalInformation(
             dayOfMonthId = dailyGoal.dayOfMonthId,
             dayGoalSummaryId = dailyGoal.dayGoalSummaryId,
             dayGoalImageId = dailyGoal.dayGoalImageId,
             dayGoalDescriptionId = dailyGoal.dayGoalDescriptionId,
-            modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium))
+            showDayGaolDescription = showDayGaolDescription,
+            modifier = Modifier
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessMediumLow
+                    )
+                )
         )
     }
 }
@@ -57,11 +87,12 @@ fun DailyGoalCard(
 
 @Composable
 fun DailyGoalInformation(
+    modifier: Modifier = Modifier,
     @StringRes dayOfMonthId: Int,
     @StringRes dayGoalSummaryId: Int,
     @DrawableRes dayGoalImageId: Int,
     @StringRes dayGoalDescriptionId: Int,
-    modifier: Modifier = Modifier
+    showDayGaolDescription: Boolean = false
 ) {
     Column(modifier = modifier) {
         Row(
@@ -93,10 +124,12 @@ fun DailyGoalInformation(
                 .clip(DailyGaolShapes.small)
 
         )
-        Text(
-            text = stringResource(dayGoalDescriptionId),
-            style = DailyGoalTypography.bodyMedium
-        )
+        if (showDayGaolDescription) {
+            Text(
+                text = stringResource(dayGoalDescriptionId),
+                style = DailyGoalTypography.bodyMedium
+            )
+        }
     }
 }
 
@@ -106,7 +139,7 @@ fun DailyGoalList(
 ) {
     LazyColumn {
         items(dailyGoals) {dailyGoal ->
-            DailyGoalCard(dailyGoal)
+            DailyGoalCard(dailyGoal = dailyGoal)
         }
     }
 }

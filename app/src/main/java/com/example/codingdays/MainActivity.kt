67cda748d53d17745e01303be6c2b908c1797bf0
moaliyou <moaliyou47@gmail.com>
2023-10-10 11:35:16 +0300

    Added top app bar to view the app name/title

--- app/src/main/java/com/example/codingdays/MainActivity.kt
+++ app/src/main/java/com/example/codingdays/MainActivity.kt
@@ -3,13 +3,29 @@ package com.example.codingdays
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Code
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.codingdays.data.DailyGoalRepository
+import androidx.compose.ui.res.dimensionResource
+import com.example.codingdays.data.DailyGoalRepository.dailyGoals
 import com.example.codingdays.ui.theme.CodingDaysTheme
+import com.example.codingdays.ui.theme.DailyGoalTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +44,52 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DailyGoalApp() {
-    DailyGoalList(DailyGoalRepository.dailyGoals)
+    Scaffold(
+        topBar = {
+            DailyGoalTopAppBar()
+        }
+    ) {
+        LazyColumn(contentPadding = it) {
+            items(dailyGoals) { dailyGoal ->
+                DailyGoalCard(
+                    Modifier
+                        .padding(
+                            top = dimensionResource(R.dimen.padding_small),
+                            bottom = dimensionResource(R.dimen.padding_small),
+                            start = dimensionResource(R.dimen.padding_medium),
+                            end = dimensionResource(R.dimen.padding_medium)
+
+                        ),
+                    dailyGoal = dailyGoal
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DailyGoalTopAppBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+            ) {
+                Icon(
+                    Icons.Filled.Code,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.top_app_icon))
+                )
+                Text(
+                    text = "30 Days Of Coding",
+                    style = DailyGoalTypography.displayLarge
+                )
+            }
+        }
+    )
 }
